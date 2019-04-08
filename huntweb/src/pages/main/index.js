2d7a4b1f9de26ca2fae29edbedbabe93d9178538
MATHEUS TORRES RIBEIRO <matheus.ribeiro@ftt-05L.engcomp.local>
2019-04-08 14:41:43 -0300

    Listando Produtos/done

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import api from "../../Services/api"
-
+import "./styles.css"
 export default class Main extends Component{
+
+    state = {
+        products: []
+    }
     componentDidMount(){
         this.loadProducts();
     }
 
     loadProducts = async ()=>{
         const response = await api.get('/products');
-        console.log(response.data.docs);
+        this.setState({products:response.data.docs});
     };
     render(){
-        return <h1>Hello Rocketseat</h1>
+        const {products} = this.state;
+        return (
+            <div className="product-list">
+            {products.map (product =>(
+                <article key= {product._id}> 
+                <strong> {product.title} </strong>
+                <p>{product.description}</p>
+                <a href="">Acessar</a>
+
+                </article>
+            ))}
+            </div>
+        );
     }
 }
