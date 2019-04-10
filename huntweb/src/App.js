c11728fcad94f02bed3d84557e210bd0aed8af71
MATHEUS TORRES RIBEIRO <matheus.ribeiro@ftt-05L.engcomp.local>
2019-04-10 14:57:19 -0300

    Tutorial quase pronto

--- huntweb/src/App.js
+++ huntweb/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './styles.css'
-import Main from './pages/main';
+
+import Routes from './routes';
+//import Main from './pages/main';
 import Header from './components/Header';
 
 const App = ()=>(
   <div className="App">
        <Header/>
-       <Main/>
+       <Routes/>
       </div>
 );
 
